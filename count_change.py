1b538f646243467b9c75d30bfe3b10d1fbc13b11
Jabroni <ranbonagarjuna@gmail.com>
2017-11-20 21:14:50 +0530

    Add files via upload

--- count_change.py
+++ count_change.py
@@ -1,15 +1,3 @@
-# def count_change(money, coins):
-#     ways = [1] + [0] * money
-#     for coin in coins:
-#         for i in range(coin, money + 1):
-#             ways[i] += ways[i - coin]
-#             print(ways)
-#     return ways[money]
-
-
-# print(count_change(4, [1, 2]))
-
-
 def count_change(money, coin_list):
     ways = [1] + [0] * money
     for coin in coin_list:
